9437c82a5296cd1c6cb22dacce4bae08a5c9c15d
Binh Nguyen <nnabinh@gmail.com>
2017-08-10 22:56:08 +0900

    types validation

--- app/App.jsx
+++ app/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
     constructor() {
@@ -29,6 +30,15 @@ class App extends React.Component {
             <div>
                 <Header></Header>
                 <Table data = {this.state.data}></Table>
+
+                <h3>Array: {this.props.propArrays}</h3>
+                <h3>Bool: {this.props.propBool ? 'True' : 'False'}</h3>
+                <h3>Func: {this.props.propFunc('This is function')}</h3>
+                <h3>Number: {this.props.propNumber}</h3>
+                <h3>String: {this.props.propString}</h3>
+                <h3>Object: {this.props.propObject.objectName1}</h3>
+                <h3>Object: {this.props.propObject.objectName2}</h3>
+                <h3>Object: {this.props.propObject.objectName3}</h3>
             </div>
         );
     }
@@ -72,5 +82,26 @@ class TableRow extends React.Component {
     }
 }
 
+App.propTypes = {
+    propArrays: PropTypes.array.isRequired,
+    propBool: PropTypes.bool.isRequired,
+    propFunc: PropTypes.func,
+    propNumber: PropTypes.number,
+    propString: PropTypes.string,
+    propObject: PropTypes.object
+}
+
+App.defaultProps = {
+    propArrays: 'this is string',
+    propBool: 'not boolean',
+    propFunc: (e) => e,
+    propNumber: 1,
+    propString: 'String value',
+    propObject: {
+        objectName1: 'Object 1',
+        objectName2: 'Object 2',
+        objectName3: 'Object 3'
+    }
+}
 
 export default App;
