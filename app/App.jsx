879aeab10e12276f1980edcb97f5445f6279a14c
Binh Nguyen <nnabinh@gmail.com>
2017-08-10 21:47:33 +0900

    Stateful management & jsconfig

--- app/App.jsx
+++ app/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data:
+            [
+                {
+                    "id":1,
+                    "name":"Foo",
+                    "age":"20"
+                },
+                {
+                    "id":2,
+                    "name":"Bar",
+                    "age":"30"
+                },		
+                {
+                    "id":3,
+                    "name":"Baz",
+                    "age":"40"
+                }
+            ]
+        }
+    }
     render() {
         return (
             <div>
-              <Header></Header>
-              <Content></Content>
+                <Header></Header>
+                <Table data = {this.state.data}></Table>
             </div>
         );
     }
@@ -23,20 +46,31 @@ class Header extends React.Component {
     }
 }
 
-class Content extends React.Component {
+class Table extends React.Component {
     render() {
-        const myStyle = {
-            fontSize: 100,
-            color: '#FF0000'
-        };
         return (
             <div>
-              <h2>Content</h2>
-              <p>This is the content</p>
-              {/* This is my comment */}
+                <table>
+                    <tbody>
+                        {this.props.data.map((person, i) => <TableRow key = {i} data = {person}></TableRow>)}
+                    </tbody>
+                </table>
             </div>
         );
     }
 }
+
+class TableRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.age}</td>
+            </tr>
+        );
+    }
+}
+
 
 export default App;
